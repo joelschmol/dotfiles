c0c996bab5dc2700f654537f01bf7c94054890fe
farnsie <88237375+joelschmol@users.noreply.github.com>
2023-11-18 21:45:51 +0500

    Add files via upload

---
+++ .config/flameshot-gui-gnome.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Run Flameshot and save the screenshot with Flameshot's default naming
+flameshot gui -c -p $HOME/Pictures/Screenshots --raw
+
+# Get the latest file in the specified directory
+latest_file=$(ls -t $HOME/Pictures/Screenshots | head -n1)
+
+# Rename the latest file with the custom naming style
+timestamp=$(date +"%Y-%m-%d %H-%M-%S")
+new_filename="Screenshot from $timestamp.png"
+mv "$HOME/Pictures/Screenshots/$latest_file" "$HOME/Pictures/Screenshots/$new_filename"
