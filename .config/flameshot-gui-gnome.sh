ee98a6f53fdd186209609454bcad531aeb169a58
farnsie <88237375+joelschmol@users.noreply.github.com>
2023-11-18 21:47:00 +0500

    Update flameshot-gui-gnome.sh

--- .config/flameshot-gui-gnome.sh
+++ .config/flameshot-gui-gnome.sh
@@ -6,7 +6,7 @@ flameshot gui -c -p $HOME/Pictures/Screenshots --raw
 # Get the latest file in the specified directory
 latest_file=$(ls -t $HOME/Pictures/Screenshots | head -n1)
 
-# Rename the latest file with the custom naming style
+# Rename the latest file with the custom naming style (same as GNOME's screen shot application)
 timestamp=$(date +"%Y-%m-%d %H-%M-%S")
 new_filename="Screenshot from $timestamp.png"
 mv "$HOME/Pictures/Screenshots/$latest_file" "$HOME/Pictures/Screenshots/$new_filename"
